222cf16690d1baf8b046be430d276ccb08c616e5
Lorne Martin <lorne@compassfoundation.io>
2020-04-20 06:06:25 -0400

    v55 done.

--- SparkAuto/Startup.cs
+++ SparkAuto/Startup.cs
@@ -40,6 +40,7 @@ namespace SparkAuto
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages().AddRazorRuntimeCompilation(); ;
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
