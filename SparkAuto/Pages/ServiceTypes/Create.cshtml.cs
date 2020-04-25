b8f74d0515e12f5beec5cd137e5c8a97397608d6
Lorne Martin <lorne@compassfoundation.io>
2020-04-25 15:36:09 -0400

    v65 done.

--- SparkAuto/Pages/ServiceTypes/Create.cshtml.cs
+++ SparkAuto/Pages/ServiceTypes/Create.cshtml.cs
@@ -4,16 +4,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using SparkAuto.Data;
 using SparkAuto.Models;
 
 namespace SparkAuto.Pages.ServiceTypes
 {
     public class CreateModel : PageModel
     {
+        private readonly SparkAuto.Data.ApplicationDbContext _db;
+
+        [BindProperty]
         public ServiceType ServiceType { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db; 
+        }
         public IActionResult OnGet()
         {
             return Page();
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.ServiceType.Add(ServiceType);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
+
     }
 }
