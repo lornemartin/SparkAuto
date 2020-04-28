79c178a0dcde83c908a1dc144ad9660a4562ff16
Lorne Martin <lorne@compassfoundation.io>
2020-04-28 06:30:25 -0400

    v85 done.

--- SparkAuto/TagHelpers/PageLinkTagHelper.cs
+++ SparkAuto/TagHelpers/PageLinkTagHelper.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
@@ -11,7 +12,7 @@ using System.Threading.Tasks;
 namespace SparkAuto.TagHelpers
 {
     [HtmlTargetElement("div",Attributes="page-model")]
-    public class PageLinkTagHelper
+    public class PageLinkTagHelper : TagHelper
     {
         private IUrlHelperFactory urlHelperFactory;
 
@@ -24,7 +25,7 @@ namespace SparkAuto.TagHelpers
         [HtmlAttributeNotBound]
         public ViewContext ViewContext { get; set; }
 
-        public PagingInfo pageModel { get; set; }
+        public PagingInfo PageModel { get; set; }
 
         public string PageAction { get; set; }
 
@@ -33,5 +34,24 @@ namespace SparkAuto.TagHelpers
         public string pageClassNormal { get; set; }
 
         public string PageClassSelected { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            TagBuilder result = new TagBuilder("div");
+           
+            for(int i=1;i<=PageModel.TotalItems;i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                string url = PageModel.Urlparam.Replace(":", i.ToString());
+                tag.Attributes["href"] = url;
+                tag.AddCssClass(PageClass);
+                tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : pageClassNormal);
+                tag.InnerHtml.Append(i.ToString());
+                result.InnerHtml.AppendHtml(tag);
+            }
+            output.Content.AppendHtml(result.InnerHtml);
+
+        }
     }
 }
