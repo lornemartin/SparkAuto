04e48d96f15f2077a925ac69fa93173c26310ec0
Lorne Martin <lorne@horstwelding.com>
2020-04-30 14:42:50 -0400

    v99 done.

--- SparkAuto/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SparkAuto/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -25,7 +25,7 @@ namespace SparkAuto.Areas.Identity.Pages.Account
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
-        //private readonly IEmailSender _emailSender;
+        private readonly IEmailSender _emailSender;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly ApplicationDbContext _db;
 
@@ -33,14 +33,14 @@ namespace SparkAuto.Areas.Identity.Pages.Account
             UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            //IEmailSender emailSender,
+            IEmailSender emailSender,
             RoleManager<IdentityRole> roleManager,
             ApplicationDbContext db)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
-            //_emailSender = emailSender;
+            _emailSender = emailSender;
             _roleManager = roleManager;
             _db = db;
         }
@@ -125,6 +125,9 @@ namespace SparkAuto.Areas.Identity.Pages.Account
                             values: new { area = "Identity", userId = user.Id, code = code },
                             protocol: Request.Scheme);
 
+                        await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
                         return RedirectToPage("/Users/Index");
                     }
                     else
@@ -133,11 +136,6 @@ namespace SparkAuto.Areas.Identity.Pages.Account
                         await _signInManager.SignInAsync(user, isPersistent: false);
                         return LocalRedirect(returnUrl);
                     }
-
-                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        //$"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    
                 }
                 foreach (var error in result.Errors)
                 {
