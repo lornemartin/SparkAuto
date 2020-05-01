e9f26e170fc3958d48fcdb843562e3edc0ef60ea
Lorne Martin <lorne@compassfoundation.io>
2020-05-01 11:46:28 -0400

    v101 assignment done

--- SparkAuto/Areas/Identity/Pages/Account/VerifyEmail.cshtml.cs
+++ SparkAuto/Areas/Identity/Pages/Account/VerifyEmail.cshtml.cs
@@ -1,9 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using SparkAuto.Data;
+using SparkAuto.Models;
+using SparkAuto.Utility;
 
 namespace SparkAuto.Areas.Identity.Pages.Account
 {
@@ -11,10 +19,39 @@ namespace SparkAuto.Areas.Identity.Pages.Account
     {
         [BindProperty]
         public string Email { get; set; }
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IEmailSender _emailSender;
+
+        public VerifyEmailModel(ApplicationDbContext db, UserManager<IdentityUser> um, IEmailSender sdr)
+        {
+            _db = db;
+            _userManager = um;
+            _emailSender = sdr;
+        }
         public IActionResult OnGet(string id)
         {
             Email = id;
             return Page();
+        }
+
+        public async Task<IActionResult> OnPost(string id)
+        {
+            var user = _db.Users.FirstOrDefault(u => u.Email==id);
+
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            var callbackUrl = Url.Page(
+                "/Account/ConfirmEmail",
+                pageHandler: null,
+                values: new { area = "Identity", userId = user.Id, code = code },
+                protocol: Request.Scheme);
+
+            await _emailSender.SendEmailAsync(user.Email, "Confirm your email",
+                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+            return RedirectToPage("/Users/Index");
+
         }
     }
 }
