fa5e24815e1e155cc4e04a8597ed7e6078a5fa53
Lorne Martin <lorne@compassfoundation.io>
2020-05-04 06:46:57 -0400

    Added ViewModel and DbContext objects

--- SparkAuto/Data/ApplicationDbContext.cs
+++ SparkAuto/Data/ApplicationDbContext.cs
@@ -18,5 +18,8 @@ namespace SparkAuto.Data
         public DbSet<ApplicationUser> ApplicationUser { get; set; }
 
         public DbSet<Car> Car { get; set; }
+        public DbSet<ServiceShoppingCart> ServiceShoppingCart { get; set; }
+        public DbSet<ServiceHeader> ServiceHeader { get; set; }
+        public DbSet<ServiceDetails> ServiceDetails { get; set; }
     }
 }
