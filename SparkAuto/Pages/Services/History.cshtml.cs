79ad85407d2f96d7fc5fc26ab7e4bdca395ec378
Lorne Martin <lorne@horstwelding.com>
2020-05-05 15:38:38 -0400

    WIP v130 done, not working yet.

--- SparkAuto/Pages/Services/History.cshtml.cs
+++ SparkAuto/Pages/Services/History.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -21,10 +22,14 @@ namespace SparkAuto.Pages.Services
 
         [BindProperty]
         public List<ServiceHeader> ServiceHeader { get; set; }
+        
+        public string UserId { get; set; }
 
         public async Task OnGet(int carId)
         {
-            ServiceHeader = await _db.ServiceHeader.Include(s => s.Car).Include(c => c.Car.ApplicationUser).Where(c => c.Car.Id == carId).ToList();
+            ServiceHeader = await _db.ServiceHeader.Include(s => s.Car).Include(c => c.Car.ApplicationUser).Where(c => c.CarId == carId).ToListAsync();
+
+            UserId = _db.Car.Where(u => u.Id == carId).ToList().FirstOrDefault().UserId;
         }
     }
 }
