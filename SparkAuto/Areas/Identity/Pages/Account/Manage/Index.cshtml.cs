c7bbbe2a3efb8d1dad6baa915e3bc1125f60882b
Lorne Martin <lorne@horstwelding.com>
2020-05-07 09:42:01 -0400

    v139 done

--- SparkAuto/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ SparkAuto/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -4,8 +4,11 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
 
 namespace SparkAuto.Areas.Identity.Pages.Account.Manage
 {
@@ -13,16 +16,25 @@ namespace SparkAuto.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IEmailSender _emailSender;
+        private readonly ApplicationDbContext _db;
 
         public IndexModel(
             UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager)
+            SignInManager<IdentityUser> signInManager,
+            IEmailSender emailSender,
+            ApplicationDbContext db)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _emailSender = emailSender;
+            _db = db;
         }
 
         public string Username { get; set; }
+
+        public bool IsEmailConfirmed { get; set; }
+
 
         [TempData]
         public string StatusMessage { get; set; }
@@ -32,23 +44,34 @@ namespace SparkAuto.Areas.Identity.Pages.Account.Manage
 
         public class InputModel
         {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+
+            public string Address { get; set; }
+            public string City { get; set; }
+            public string PostalCode { get; set; }
         }
 
-        private async Task LoadAsync(IdentityUser user)
-        {
-            var userName = await _userManager.GetUserNameAsync(user);
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+        //private async Task LoadAsync(IdentityUser user)
+        //{
+        //    var userName = await _userManager.GetUserNameAsync(user);
+        //    var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
 
-            Username = userName;
+        //    Username = userName;
 
-            Input = new InputModel
-            {
-                PhoneNumber = phoneNumber
-            };
-        }
+        //    Input = new InputModel
+        //    {
+        //        PhoneNumber = phoneNumber
+        //    };
+        //}
 
         public async Task<IActionResult> OnGetAsync()
         {
@@ -58,34 +81,47 @@ namespace SparkAuto.Areas.Identity.Pages.Account.Manage
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
-            await LoadAsync(user);
+            var userFromDb = await _db.ApplicationUser.FirstOrDefaultAsync(u => u.Email == user.Email);
+
+
+            Username = userFromDb.UserName;
+
+            Input = new InputModel
+            {
+                Email = userFromDb.Email,
+                PhoneNumber = userFromDb.PhoneNumber,
+                Address = userFromDb.Address,
+                City = userFromDb.City,
+                Name = userFromDb.Name,
+                PostalCode = userFromDb.PostalCode,
+            };
+
+            IsEmailConfirmed = await _userManager.IsEmailConfirmedAsync(user);
+
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
-            if (!ModelState.IsValid)
-            {
-                await LoadAsync(user);
-                return Page();
-            }
+            var userFromDb = await _db.ApplicationUser.FirstOrDefaultAsync(u => u.Email == user.Email);
 
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            if (Input.PhoneNumber != phoneNumber)
-            {
-                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
-                if (!setPhoneResult.Succeeded)
-                {
-                    var userId = await _userManager.GetUserIdAsync(user);
-                    throw new InvalidOperationException($"Unexpected error occurred setting phone number for user with ID '{userId}'.");
-                }
-            }
+            userFromDb.Name = Input.Name;
+            userFromDb.Address = Input.Address;
+            userFromDb.City = Input.City;
+            userFromDb.PostalCode = Input.PostalCode;
+            userFromDb.PhoneNumber = Input.PhoneNumber;
+            await _db.SaveChangesAsync();
 
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
